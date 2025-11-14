8cabc818decf3b8486db73d616a1985e0c97b18d
Samantha Castillo <samanthacas@pm.me>
2025-11-13 18:58:39 -0800

    Adds more styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,10 @@
 export default function Main() {
   return (
     <main>
-      <div className="main-box"></div>
-      <h1 className="message">Message Here</h1>
+      <div className="main-box">
+        <p>#FFFFFF</p>
+      </div>
+      <h2 className="message">Incorrect!<br />Try again</h2>
       <button className="buttonOne">#FFFFFF</button>
       <button className="buttonTwo">#FFFFFF</button>
       <button className="buttonThree">#FFFFFF</button>
